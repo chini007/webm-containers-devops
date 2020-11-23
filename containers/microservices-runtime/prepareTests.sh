6cbf53c388219fefa0ed456ce947b953c68dfef1
EUR\YSI <ysi@softwareag.com>
2020-11-23 16:54:46 +0200

    cleanup

--- containers/microservices-runtime/prepareTests.sh
+++ containers/microservices-runtime/prepareTests.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 CONTAINER_ID=`docker ps -q`
-docker exec -t $CONTAINER_ID ls -lR
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/ext/enttoolkit.jar .
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/glassfish/gf.jakarta.mail.jar .
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/wm-isclient.jar .
