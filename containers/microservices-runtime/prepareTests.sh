395e560dee03a9a53aa214e72370110cb20713a4
EUR\YSI <ysi@softwareag.com>
2020-11-24 09:07:07 +0200

    Add logging in the shell script

--- containers/microservices-runtime/prepareTests.sh
+++ containers/microservices-runtime/prepareTests.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -x
 CONTAINER_ID=`docker ps -q`
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/ext/enttoolkit.jar .
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/glassfish/gf.jakarta.mail.jar .
 docker cp $CONTAINER_ID:/opt/softwareag/common/lib/wm-isclient.jar .
 docker cp $CONTAINER_ID:/opt/softwareag/IntegrationServer/lib/wm-isserver.jar .
+ls -la
