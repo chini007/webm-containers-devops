b5712c22a8906d0b62454ed10046cd1bc807aa94
EUR\YSI <ysi@softwareag.com>
2020-11-24 09:29:25 +0200

    Play with classpath for tests

--- containers/microservices-runtime/prepareTests.sh
+++ containers/microservices-runtime/prepareTests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 CONTAINER_ID=`docker ps -q`
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/ext/enttoolkit.jar .
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/glassfish/gf.jakarta.mail.jar .
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/wm-isclient.jar .
-docker cp $CONTAINER_ID:/opt/softwareag/IntegrationServer/lib/wm-isserver.jar .
-ls -la
+docker cp $CONTAINER_ID:/opt/softwareag/common/lib/ext/enttoolkit.jar ../../lib
+docker cp $CONTAINER_ID:/opt/softwareag/common/lib/glassfish/gf.jakarta.mail.jar ../../lib
+docker cp $CONTAINER_ID:/opt/softwareag/common/lib/wm-isclient.jar ../../lib
+docker cp $CONTAINER_ID:/opt/softwareag/IntegrationServer/lib/wm-isserver.jar ../../lib
+ls -la ../../lib
