bcd1b2a33f6276237417c4a07a2126e899a52530
Angel Tcholtchev <angel.tcholtchev@softwareag.com>
2021-03-17 17:54:04 +0200

    Delete prepareTests.sh
    remove unused script

--- containers/microservices-runtime/prepareTests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-CONTAINER_ID=`docker ps -q`
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/ext/enttoolkit.jar ../../lib
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/glassfish/gf.jakarta.mail.jar ../../lib
-docker cp $CONTAINER_ID:/opt/softwareag/common/lib/wm-isclient.jar ../../lib
-docker cp $CONTAINER_ID:/opt/softwareag/IntegrationServer/lib/wm-isserver.jar ../../lib
-ls -la ../../lib
